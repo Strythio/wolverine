2343cb79d5847bc26933670a3cf8abff6036f06f
Armus Daube <armusdaube@gmail.com>
2026-07-21 20:47:39 -0400

    Added support for natural key handling in `UpdatedAggregate` response configuration.

--- src/Persistence/Wolverine.Marten/UpdatedAggregate.cs
+++ src/Persistence/Wolverine.Marten/UpdatedAggregate.cs
@@ -15,24 +15,41 @@ namespace Wolverine.Marten;
 /// to response with the updated version of the aggregate being
 /// altered *after* any new events have been applied
 /// </summary>
-public class UpdatedAggregate : IResponseAware
-{
+public class UpdatedAggregate : IResponseAware {
     public static void ConfigureResponse(IChain chain)
     {
-        if (AggregateHandling.TryLoad(chain, out var handling))
+        if (AggregateHandling.TryLoad(
+                chain,
+                out var handling))
         {
             var idType = handling.AggregateId.VariableType;
-            
+
             // TODO -- with https://github.com/JasperFx/wolverine/issues/1167, this might need to try to create value
             // type first
-            var openType = idType == typeof(Guid) ? typeof(FetchLatestByGuid<>) : typeof(FetchLatestByString<>);
-            var frame = openType.CloseAndBuildAs<MethodCall>(handling.AggregateId, handling.AggregateType);
+            if (handling.IsNaturalKey)
+            {
+                var valueTypeIdType = typeof(FetchLatestByNaturalKey<,>).CloseAndBuildAs<MethodCall>(
+                    handling.AggregateId,
+                    handling.AggregateType,
+                    idType);
+                chain.UseForResponse(valueTypeIdType);
+            }
+            else
+            {
+                var simpleOpenType = idType == typeof(Guid)
+                    ? typeof(FetchLatestByGuid<>)
+                    : typeof(FetchLatestByString<>);
+                var frame = simpleOpenType.CloseAndBuildAs<MethodCall>(
+                    handling.AggregateId,
+                    handling.AggregateType);
 
-            chain.UseForResponse(frame);
+                chain.UseForResponse(frame);
+            }
         }
         else
         {
-            throw new InvalidOperationException($"UpdatedAggregate cannot be used because Chain {chain} is not marked as an aggregate handler. Are you missing an [AggregateHandler] or [Aggregate] attribute on the handler?");
+            throw new InvalidOperationException(
+                $"UpdatedAggregate cannot be used because Chain {chain} is not marked as an aggregate handler. Are you missing an [AggregateHandler] or [Aggregate] attribute on the handler?");
         }
 
     }
@@ -45,53 +62,93 @@ public class UpdatedAggregate : IResponseAware
 /// altered *after* any new events have been applied
 /// </summary>
 /// <typeparam name="T">The aggregate type. Use this version of UpdatedAggregate if you need to help Wolverine "know" which of multiple event streams should be the "updated aggregate"</typeparam>
-public class UpdatedAggregate<T> : IResponseAware
-{
+public class UpdatedAggregate<T> : IResponseAware {
     public static void ConfigureResponse(IChain chain)
     {
-        if (AggregateHandling.TryLoad<T>(chain, out var handling))
+        if (AggregateHandling.TryLoad<T>(
+                chain,
+                out var handling))
         {
             var idType = handling.AggregateId.VariableType;
-            
-            // TODO -- with https://github.com/JasperFx/wolverine/issues/1167, this might need to try to create value
-            // type first
-            var openType = idType == typeof(Guid) ? typeof(FetchLatestByGuid<>) : typeof(FetchLatestByString<>);
-            var frame = openType.CloseAndBuildAs<MethodCall>(handling.AggregateId, handling.AggregateType);
 
-            chain.UseForResponse(frame);
+            // Handle case where the aggregate is discovered via a natural key
+            if (handling.IsNaturalKey)
+            {
+                var valueTypeIdType = typeof(FetchLatestByNaturalKey<,>).CloseAndBuildAs<MethodCall>(
+                    handling.AggregateId,
+                    handling.AggregateType,
+                    idType);
+                chain.UseForResponse(valueTypeIdType);
+            }
+            else
+            {
+                var simpleOpenType = idType == typeof(Guid)
+                    ? typeof(FetchLatestByGuid<>)
+                    : typeof(FetchLatestByString<>);
+                var frame = simpleOpenType.CloseAndBuildAs<MethodCall>(
+                    handling.AggregateId,
+                    handling.AggregateType);
+
+                chain.UseForResponse(frame);
+            }
         }
         else
         {
-            throw new InvalidOperationException($"UpdatedAggregate cannot be used because Chain {chain} is not marked as an aggregate handler. Are you missing an [AggregateHandler] or [Aggregate] attribute on the handler?");
+            throw new InvalidOperationException(
+                $"UpdatedAggregate cannot be used because Chain {chain} is not marked as an aggregate handler. Are you missing an [AggregateHandler] or [Aggregate] attribute on the handler?");
         }
 
     }
 }
 
-internal class FetchLatestByGuid<T> : MethodCall where T : class
-{
-    public FetchLatestByGuid(Variable id) : base(typeof(IEventStoreOperations), ReflectionHelper.GetMethod<IEventStoreOperations>(x => x.FetchLatest<T>(Guid.Empty, CancellationToken.None))!)
+internal class FetchLatestByGuid<T> : MethodCall
+    where T : class {
+    public FetchLatestByGuid(Variable id) : base(
+        typeof(IEventStoreOperations),
+        ReflectionHelper.GetMethod<IEventStoreOperations>(x => x.FetchLatest<T>(
+            Guid.Empty,
+            CancellationToken.None))!)
     {
         if (id.VariableType != typeof(Guid))
         {
             throw new ArgumentOutOfRangeException(
                 "Wolverine does not yet support strong typed identifiers for the aggregate workflow. See https://github.com/JasperFx/wolverine/issues/1167");
         }
-        
+
         Arguments[0] = id;
     }
 }
 
-internal class FetchLatestByString<T> : MethodCall where T : class
-{
-    public FetchLatestByString(Variable id) : base(typeof(IEventStoreOperations), ReflectionHelper.GetMethod<IEventStoreOperations>(x => x.FetchLatest<T>("", CancellationToken.None))!)
+internal class FetchLatestByString<T> : MethodCall
+    where T : class {
+    public FetchLatestByString(Variable id) : base(
+        typeof(IEventStoreOperations),
+        ReflectionHelper.GetMethod<IEventStoreOperations>(x => x.FetchLatest<T>(
+            "",
+            CancellationToken.None))!)
     {
         if (id.VariableType != typeof(string))
         {
             throw new ArgumentOutOfRangeException(
                 "Wolverine does not yet support strong typed identifiers for the aggregate workflow. See https://github.com/JasperFx/wolverine/issues/1167");
         }
-        
+
+        Arguments[0] = id;
+    }
+}
+
+internal class FetchLatestByNaturalKey<T, TId> : MethodCall
+    where T : class
+    where TId : notnull {
+    public FetchLatestByNaturalKey(Variable id) : base(
+        typeof(IEventStoreOperations),
+        ReflectionHelper.GetMethod<IEventStoreOperations>(x => x.FetchLatest<T, TId>(
+            // Note that libraries like Vogen won't allow using Activator on its generated value types. 
+            // However, that is enforced via an analyzer; since this code is only used for reflection purposes
+            // and is never actually executed, there should be no issue.
+            Activator.CreateInstance<TId>(),
+            CancellationToken.None))!)
+    {
         Arguments[0] = id;
     }
 }
